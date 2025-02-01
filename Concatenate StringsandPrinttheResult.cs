4b11b3b6c4f917e22fcb84d0c7722ca92e0797f9
shahidkm <kmshahid432@gmail.com>
2025-02-01 17:06:27 +0530

    Add files via upload

---
+++ Concatenate StringsandPrinttheResult.cs
@@ -0,0 +1,10 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Hello, ";
+        string str2 = "World!";
+        string result = str1 + str2;
+        Console.WriteLine(result);
+    }
+}
